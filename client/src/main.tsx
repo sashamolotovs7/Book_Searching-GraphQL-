5855d07e90a0fc36641fb43f856bcfaeb51877b0
sashamolotovs7 <alekcvolkov7@gmail.com>
2024-10-15 15:20:23 -0400

    Initial commit with GraphQL refactoring

---
+++ client/src/main.tsx
@@ -0,0 +1,38 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: '/saved',
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
