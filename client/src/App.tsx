ff584e6395683e0290a2fd5e584493ad3983825a
sashamolotovs7 <alekcvolkov7@gmail.com>
2024-10-21 17:45:53 -0400

    running client/server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { useEffect } from 'react';
 
 // Set up the HTTP link for Apollo Client to communicate with the GraphQL server
 const httpLink = createHttpLink({
@@ -13,7 +14,7 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
   // Retrieve the JWT token from localStorage
   const token = localStorage.getItem('id_token');
-  
+
   // Return the headers with the Authorization token included
   return {
     headers: {
@@ -29,10 +30,21 @@ const client = new ApolloClient({
   cache: new InMemoryCache(), // Set up cache management
 });
 
+// The main App component
 function App() {
+  useEffect(() => {
+    // Log to help confirm that the token is correctly set during app initialization
+    const token = localStorage.getItem('id_token');
+    if (token) {
+      console.log('JWT Token available: ', token);
+    } else {
+      console.warn('No JWT token found. User might not be authenticated.');
+    }
+  }, []); // Run once when the app mounts
+
   return (
     <ApolloProvider client={client}> {/* Provide the Apollo client to the app */}
-      <Navbar />
+      <Navbar /> {/* Navbar component */}
       <Outlet /> {/* Render the current page */}
     </ApolloProvider>
   );
